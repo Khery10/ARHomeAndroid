f5175e2c4071f0200bc91439be3db138aa631b96
Khery10 <danil201025@gmail.com>
2021-04-26 23:43:01 +0500

    ARHomeAndroid - made saving image result

--- app/src/main/java/com/arhome/views/camera/ImageViewerFragment.kt
+++ app/src/main/java/com/arhome/views/camera/ImageViewerFragment.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.arhome.R
 import com.arhome.databinding.ImageViewerFragmentBinding
+import com.arhome.utils.io.saveJpgImageToGallery
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -24,43 +24,16 @@ import kotlinx.android.synthetic.main.image_viewer_fragment.view.*
 
 class ImageViewerFragment : Fragment() {
 
-    private val navController: NavController by lazy {
-        findNavController()
-    }
-
-    /** AndroidX navigation arguments */
-    private val args: ImageViewerFragmentArgs by navArgs()
+    private val _args: ImageViewerFragmentArgs by navArgs()
 
     private val _loaded: MutableLiveData<Boolean> = MutableLiveData(false)
 
-    lateinit var binding: ImageViewerFragmentBinding
+    private lateinit var _binding: ImageViewerFragmentBinding
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
+    private var _saved = false
 
-        binding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.image_viewer_fragment,
-                container,
-                false
-        )
-
-        binding.loaded = _loaded
-        binding.lifecycleOwner = viewLifecycleOwner
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        back_to_previous_button.setOnClickListener {
-            findNavController().popBackStack()
-        }
-
-        Glide.with(view.imageViewer).load(args.filePath).listener(object : RequestListener<Drawable> {
+    private val _loadListener: RequestListener<Drawable> by lazy {
+        object : RequestListener<Drawable> {
 
             override fun onLoadFailed(
                     e: GlideException?,
@@ -81,7 +54,48 @@ class ImageViewerFragment : Fragment() {
                 _loaded.postValue(true)
                 return false
             }
+        }
+    }
 
-        }).into(view.imageViewer)
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.image_viewer_fragment,
+                container,
+                false
+        )
+
+        _binding.loaded = _loaded
+        _binding.lifecycleOwner = viewLifecycleOwner
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        back_to_previous_button.setOnClickListener { findNavController().popBackStack() }
+        save_image_button.setOnClickListener { saveImageView() }
+
+        Glide.with(view.imageViewer)
+                .load(_args.filePath)
+                .listener(_loadListener)
+                .into(view.imageViewer)
+    }
+
+    private fun saveImageView() {
+
+        if (!_saved) {
+            saveJpgImageToGallery(requireContext(), _args.filePath)
+
+            _saved = true
+            Toast.makeText(requireContext(), "Saved", Toast.LENGTH_SHORT).show()
+        } else
+            Toast.makeText(requireContext(), "Image already saved", Toast.LENGTH_SHORT).show()
+
     }
 }
